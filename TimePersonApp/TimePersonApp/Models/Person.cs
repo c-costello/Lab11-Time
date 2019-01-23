081da5669328b084759f48591f510ba0617a46db
Clarice Costello <claricostello@outlook.com>
2019-01-23 09:26:02 -0800

    created Person class

---
+++ TimePersonApp/TimePersonApp/Models/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TimePersonApp.Models
+{
+    public class Person
+    {
+        public int Year { get; set; }
+        public string Honor { get; set; }
+        public string Name { get; set; }
+        public string Country {get; set;}
+        public int BirthYear { get; set; }
+        public int DeathYear { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Context { get; set; }
+    }
+}
